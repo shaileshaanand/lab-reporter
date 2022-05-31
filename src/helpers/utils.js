b556e3839026d050131e456cd8bf4f05bcebf759
Shailesh Aanand <anaandshailu@gmail.com>
2022-05-31 13:19:37 +0530

    fix sanitize on nested models

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,8 +1,15 @@
 const sanitize = (obj) => {
-  obj.id = obj._id;
+  if (obj._id) {
+    obj.id = obj._id;
+  }
   delete obj._id;
   delete obj.__v;
   "deleted" in obj && delete obj.deleted;
+  for (const key in obj) {
+    if (!["id", "$__"].includes(key) && typeof obj[key] === "object") {
+      obj[key] = sanitize(obj[key]);
+    }
+  }
   return obj;
 };
 
