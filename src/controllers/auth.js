dab7473b51b406f9be1cff08498da33ef9747ed2
Shailesh Aanand <anaandshailu@gmail.com>
2022-06-06 23:14:48 +0530

    add validation to login route

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,8 +1,15 @@
+const Joi = require("joi");
+
 const { NotFoundError, UnauthenticatedError } = require("../errors");
 const { sanitize } = require("../helpers/utils");
 const { User } = require("../models");
 
 const login = async (req, res) => {
+  const bodyValidator = Joi.object({
+    username: Joi.string().required(),
+    password: Joi.string().required(),
+  });
+  Joi.assert(req.body, bodyValidator);
   const { username, password } = req.body;
   const user = await User.findOne({ username }).select("+password");
   if (!user) {
