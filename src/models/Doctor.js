e5b16424fbf1417776a499d599e95da0e2a07e78
Shailesh Aanand <anaandshailu@gmail.com>
2022-06-18 17:29:44 +0530

    Add timestamps to Doctpr

--- src/models/Doctor.js
+++ src/models/Doctor.js
@@ -1,29 +1,34 @@
 const mongoose = require("mongoose");
 
-const DoctorSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    maxlength: 255,
-  },
-  phone: {
-    type: String,
-    required: true,
-    validate: {
-      validator: function (v) {
-        return /^[6-9]\d{9}$/.test(v);
+const DoctorSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      maxlength: 255,
+    },
+    phone: {
+      type: String,
+      required: true,
+      validate: {
+        validator: function (v) {
+          return /^[6-9]\d{9}$/.test(v);
+        },
+        message: "Phone number must be 10 digits, starting with 6,7,8 or 9",
       },
-      message: "Phone number must be 10 digits, starting with 6,7,8 or 9",
+    },
+    email: {
+      type: String,
+      required: false,
+    },
+    deleted: {
+      type: Boolean,
+      default: false,
     },
   },
-  email: {
-    type: String,
-    required: false,
+  {
+    timestamps: true,
   },
-  deleted: {
-    type: Boolean,
-    default: false,
-  },
-});
+);
 
 module.exports = mongoose.model("Doctor", DoctorSchema);
