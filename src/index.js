a3ee2c095570f17909830762e0c6da05c11ce9f9
Shailesh Aanand <anaandshailu@gmail.com>
2022-08-26 20:07:54 +0530

    add graceful exit for docker

--- src/index.js
+++ src/index.js
@@ -10,8 +10,19 @@ const start = async () => {
   console.log(`Connecting to MongoDB at ${mongo_uri}`);
   await dbConnect(mongo_uri);
   console.log(`Connected to MongoDB at ${mongo_uri}`);
-  await app.listen(port);
+  const server = await app.listen(port);
   console.log(`Server started on port ${port}`);
+
+  const shutdown = () => {
+    console.log("Stopping ...");
+    server.close(() => {
+      console.log("Stopped");
+      process.exit();
+    });
+  };
+
+  process.on("SIGINT", shutdown);
+  process.on("SIGTERM", shutdown);
 };
 
 start();
